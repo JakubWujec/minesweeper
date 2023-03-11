d6dc3d308e980dcef0285840fc9dca9665dcdfb7
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-03-11 14:22:43 +0100

    start new game after change of settings

--- scripts/game/controller/controller.js
+++ scripts/game/controller/controller.js
@@ -1,4 +1,4 @@
-class Controller{
+class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
@@ -8,8 +8,8 @@ class Controller{
         rows: 8,
         columns: 8,
         mines: 10
-      }, 
-      'MEDIUM' : {
+      },
+      'MEDIUM': {
         rows: 16,
         columns: 16,
         mines: 40
@@ -31,13 +31,13 @@ class Controller{
     this.flagMode = false;
   }
 
-  handleCanvasClick(event){
+  handleCanvasClick(event) {
     console.log(event);
-    if(!this.model.isGameFinished()){
+    if (!this.model.isGameFinished()) {
       let location = this.view.getRowAndColOfClick(event);
       console.log(location);
-      if(location !== null){
-        if(this.flagMode){
+      if (location !== null) {
+        if (this.flagMode) {
           this.model.board.getCellAt(...location).toggleFlag();
         } else {
           this.model.board.selectCell(...location);
@@ -47,72 +47,73 @@ class Controller{
       this.view.displayBoard(this.model.board);
       this.view.setMinesCounter(this.model.board.getCoveredFlaggedCells().length, this.model.board.initialNumberOfMines);
 
-      if(this.model.isGameWon()){
+      if (this.model.isGameWon()) {
         this.handleGameWon();
-      } else if (this.model.isGameLost()){
+      } else if (this.model.isGameLost()) {
         this.handleGameLost();
       }
     }
   }
 
-  handleGameLost(){
+  handleGameLost() {
     this.view.alertOnGameLost();
   }
 
-  handleGameWon(){
+  handleGameWon() {
     this.view.alertOnGameWon();
   }
 
-  handleToggleFlagMode(){
+  handleToggleFlagMode() {
     this.flagMode = !this.flagMode;
     this.view.toggleFlagButton();
   }
 
-  handleStartGame(){
+  handleStartGame() {
     let settings = this.getSettings();
     console.log(settings);
-    if(settings.rows > 0 && settings.columns > 0 && settings.mines > 0 && (settings.rows * settings.columns) > settings.mines){
+    if (settings.rows > 0 && settings.columns > 0 && settings.mines > 0 && (settings.rows * settings.columns) > settings.mines) {
       this.model = new Model(settings.rows, settings.columns, settings.mines);
       this.view = new View(settings.rows, settings.columns);
       this.view.displayBoard(this.model.board);
       this.view.setMinesCounter(0, settings.mines);
-      if(this.flagMode){
+      if (this.flagMode) {
         this.handleToggleFlagMode();
       }
     }
   }
 
-  handleSaveSettings(settings){
+  handleSaveSettings(settings) {
     localStorage.setItem('settings', JSON.stringify(settings));
+    this.handleStartGame();
   }
 
-  handleSettingsButtonClick(){
+  handleSettingsButtonClick() {
     this.view.openSettingsModal();
     let settings = this.getSettings();
     this.view.setSettingsModalValues(settings.rows, settings.columns, settings.mines);
   }
 
-  handleDifficultyLevelClick(levelName){
-    if(levelName in this.levelSettings){
+  handleDifficultyLevelClick(levelName) {
+    if (levelName in this.levelSettings) {
       this.view.setSettingsModalValues(
         this.levelSettings[levelName].rows,
         this.levelSettings[levelName].columns,
         this.levelSettings[levelName].mines
       );
     }
-    
+
   }
 
-  getSettings(){
+  getSettings() {
     let settings = this.levelSettings['EASY'];
     let lsSettings = JSON.parse(localStorage.getItem('settings'));
-    if(lsSettings){
+    if (lsSettings) {
       settings = lsSettings;
     }
     return settings;
   }
 }
 
-let app = new Controller(new Model(8,8,10), new View(8,8));
-app.handleStartGame(8,8,10);
+let app = new Controller(new Model(8, 8, 10), new View(8, 8));
+app.handleStartGame(8, 8, 10);
 
